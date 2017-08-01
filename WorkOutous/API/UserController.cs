7109c16cb4fe8f5fe1e025ebbbf67e66c90ebe22
unknown <Omer>
2017-08-01 13:34:20 -0700

    Omers Commit 7/31/2017 UPDATED

--- WorkOutous/API/UserController.cs
+++ WorkOutous/API/UserController.cs
@@ -8,6 +8,7 @@ using WorkOutous.Models;
 using WorkOutous.ViewModels;
 
 
+//
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WorkOutous.API
