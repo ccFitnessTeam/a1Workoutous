724f809a10755593fcca6f2020497c78388c763f
Kristofer Inniss <kristofer.inniss@codercamps.com>
2017-08-14 13:00:24 -0700

    comments

--- WorkOutous/wwwroot/ngApp/controllers/workoutController.js
+++ WorkOutous/wwwroot/ngApp/controllers/workoutController.js
@@ -1,17 +1,21 @@
 ﻿
 class WorkoutController {
     constructor($workout) {
+        //set service to propery
         this.workoutService = $workout;
+        //set prop for getting all workouts
         this.workouts;
+        //assign data via get method
         this.getAllWorkouts().then((res) => {
             this.workouts = res.data;
         });
+        //set prop for single workout
         this.workout = {
             name: "",
             exercises: []
         };
     }
-
+    
     getAllWorkouts() {
         this.workoutService.getAll();
     }
